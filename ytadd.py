ae41db34da2ab22346ddc330b7f20871f9a096b1
Ashton <ashton.ccross@gmail.com>
2021-10-04 10:04:09 -0600

    Removed unecessary 'mode' feature. Served no purpose.

--- ytadd.py
+++ ytadd.py
@@ -10,44 +10,32 @@ newsboat_url = os.path.expanduser('~/.newsboat/urls')           # replace this w
 
 # -------------------------------
 
+valid_input = True
+
 try:
-    mode = sys.argv[1]
+    channel_id = ('https://www.youtube.com/feeds/videos.xml?channel_id=' + sys.argv[1])
 except:
-    mode = None
+    print('Please enter a youtube Channel ID.')
+    valid_input = False
 
-if mode == 'help':
-    print('help mode coming soon')
-elif mode == 'tags':
-    print('tags coming soon')
-else:
-    # Add mode
+tags = []
 
-    valid_input = True
+if valid_input:
+
+    for i in range(2, 100, 1):
+        try:
+            tags.append('"' + sys.argv[i] + '"')
+        except:
+            break
+
+    tag_output = ''
+    for t in tags:
+        tag_output += ' ' + t
 
     try:
-        channel_id = ('https://www.youtube.com/feeds/videos.xml?channel_id=' + sys.argv[1])
-    except:
-        print('Please enter a youtube Channel ID.')
-        valid_input = False
-
-    tags = []
-
-    if valid_input:
-
-        for i in range(2, 100, 1):
-            try:
-                tags.append('"' + sys.argv[i] + '"')
-            except:
-                break
-
-        tag_output = ''
-        for t in tags:
-            tag_output += ' ' + t
-
-        try:
-            url_file = open(newsboat_url, 'a')
-            url_file.write(f'{channel_id} {tag_output} \n')
-            url_file.close()
-            print(f'Sucessfully added:\n \'{channel_id + tag_output}\'\n            to\n \'{newsboat_url}\'')
-        except FileNotFoundError:
-            print('Couldn\'t find the specified URL file.')
+        url_file = open(newsboat_url, 'a')
+        url_file.write(f'{channel_id} {tag_output} \n')
+        url_file.close()
+        print(f'Sucessfully added:\n \'{channel_id + tag_output}\'\n            to\n \'{newsboat_url}\'')
+    except FileNotFoundError:
+        print('Couldn\'t find the specified URL file.')
